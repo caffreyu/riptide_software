c4a1646c2f49daef99a86c51bf72228484a948ba
caffreyu <caffreyu@gmail.com>
2019-07-19 11:33:30 -0400

    modified cfg file

--- riptide_controllers/scripts/alignment_controller.py
+++ riptide_controllers/scripts/alignment_controller.py
@@ -3,8 +3,8 @@
 import rospy
 from riptide_msgs.msg import AlignmentCommand, DepthCommand, LinearCommand
 from darknet_ros_msgs.msg import BoundingBoxes
-from darknet_ros_msgs import BoundingBox
-from std_msgs.msg import Float32
+"""from darknet_ros_msgs import BoundingBox"""
+from std_msgs.msg import Float64
 from geometry_msgs.msg import Vector3
 from dynamic_reconfigure.server import Server
 from riptide_controllers.cfg import AlignmentControllerConfig
@@ -18,16 +18,23 @@ class AlignmentController():
     Y_FORCE_P = 2.0
     Z_FORCE_P = 2.0
 
-    cam_width = 644;
-    cam_height = 482;
+    cam_width = 644
+    cam_height = 482
     currentCam = 0
 
+    """
     x_error = 0
     y_error = 0
     z_error = 0
+    """
+    error = Vector3(0, 0, 0)
+
+    """
     x_forceCmd = 0
     y_forceCmd = 0
     z_forceCmd = 0
+    """
+    force = Vector3(0, 0, 0)
 
     width_ratio = 0
     
@@ -42,27 +49,27 @@ class AlignmentController():
         self.width_ratio = msg.width_ratio
         for bbox in msg.bounding_boxes:
             if self.object == bbox.Class:
-                self.x_error = ((bbox.xmin + bbox.xmax) / 2 - self.cam_width / 2)
-                self.y_error = ((bbox.ymin + bbox.ymax) / 2 - self.cam_height / 2)
-                self.z_error = ((bbox.xmax - bbox.xmin) - self.cam_width)
+                self.error.x = ((bbox.xmin + bbox.xmax) / 2 - self.cam_width / 2)
+                self.error.y = ((bbox.ymin + bbox.ymax) / 2 - self.cam_height / 2)
+                self.error.z = ((bbox.xmax - bbox.xmin) - self.cam_width)
         
-        self.x_forceCmd = X_FORCE_P * self.x_error
-        self.y_forceCmd = Y_FORCE_P * self.y_error
-        self.z_forceCmd = Z_FORCE_P * self.z_error
-        if self.cam == 0:
-            YPub.publish(self.x_forceCmd, LinearCommand.FORCE)
-            XPub.publish(-self.z_forceCmd, LinearCommand.FORCE)
-            depthPub.publish(-self.y_forceCmd, DepthCommand.depth)
+        self.force.x = self.X_FORCE_P * self.error.x
+        self.force.y = self.Y_FORCE_P * self.error.y
+        self.force.z = self.Z_FORCE_P * self.error.z
+        if self.currentCam == 0:
+            YPub.publish(self.force.x, LinearCommand.FORCE)
+            XPub.publish(-self.force.z, LinearCommand.FORCE)
+            depthPub.publish(-self.force.y, DepthCommand.depth)
         else:
-            YPub.publish(self.x_forceCmd, LinearCommand.FORCE)
-            XPub.publish(self.y_forceCmd, LinearCommand.FORCE)
-            depthPub.publish(-self.z_forceCmd, DepthCommand.depth)
+            YPub.publish(self.force.x, LinearCommand.FORCE)
+            XPub.publish(self.force.y, LinearCommand.FORCE)
+            depthPub.publish(-self.force.z, DepthCommand.depth)
 
     def reconfigure(self, config, name):
-        self.X_FORCE_P = config[name + "x_force_p"]
-        self.Y_FORCE_P = config[name + "y_force_p"]
-        self.Z_FORCE_P = config[name + "z_force_p"]
-        self.MAX_FORCE = config[name + "max_force"]
+        self.X_FORCE_P = config[name + "_x_force_p"]
+        self.Y_FORCE_P = config[name + "_y_force_p"]
+        self.Z_FORCE_P = config[name + "_z_force_p"]
+        self.MAX_FORCE = config[name + "_max_force"]
         
 alignmentController = AlignmentController()
 
@@ -70,12 +77,10 @@ XPub = rospy.Publisher("/command/x", Float64, queue_size=5)
 YPub = rospy.Publisher("/command/y", Float64, queue_size=5)
 depthPub = rospy.Publisher("/command/depth", DepthCommand, queue_size=5)
 
-   
 def dynamicReconfigureCb(config, level):
     # On dynamic reconfiguration
-    alignmentController.reconfigure(config, "alignment")
+    alignmentController.reconfigure(config, "align")
     return config
-
 
 if __name__ == '__main__':
 
